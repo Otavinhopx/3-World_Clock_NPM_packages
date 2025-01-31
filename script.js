9ecf2e253641e92b1c1fb3cd87c674b528a93875
Otávio <otavioaugusto42@hotmail.com>
2025-01-31 17:26:11 -0300

    Adicionando a escolha do fuso horário com timezone

--- script.js
+++ script.js
@@ -1,22 +1,56 @@
+const dateHour = document.getElementById("dateHour");
 const dateDay = document.getElementById("dateDay")
+const timezoneSelector = document.querySelector(".modal_list");
+const applyButton = document.querySelector(".modal_apply");
+const timeZone = document.getElementById("timeZone")
 
+dayjs.extend(dayjs_plugin_utc);
+dayjs.extend(dayjs_plugin_timezone);
+
+let selectedTimezone = "America/Sao_Paulo";
+timeZone.textContent = "Brasilia (BRT UTC-3)";
 
 
 const date = dayjs().format('dddd, DD MMMM, YYYY')
 
 function updateClock(){
-    const hour = dayjs().format('HH:mm:ss')
-    document.getElementById("dateHour").textContent = hour;
+    const now = dayjs().tz(selectedTimezone);
+    dateHour.textContent = now.format('HH:mm:ss');
 }
 
 
 setInterval(updateClock, 1000);
 updateClock();
-dateDay.innerHTML += date
+
+dateDay.textContent = dayjs().tz(selectedTimezone).format('dddd, DD MMMM, YYYY');
+
+applyButton.addEventListener("click", () => {
+    const timezoneMap = {
+        "Brasilia (BRT UTC-3)": "America/Sao_Paulo",
+        "New York (CST UTC-6)": "America/Chicago",
+        "Spain (CET UTC+1)": "Europe/Madrid",
+        "Japan (JST UTC+9)": "Asia/Tokyo",
+        "Australia (ACT UTC+9:30/+10:30)": "Australia/Sydney"
+    };
+    const selectedOption = timezoneSelector.value;
+    if (timezoneMap[selectedOption]) {
+        selectedTimezone = timezoneMap[selectedOption];
+        timeZone.textContent = selectedOption;
+        updateClock(); // Atualizar o relógio imediatamente
+        dateDay.textContent = dayjs().tz(selectedTimezone).format('dddd, DD MMMM, YYYY');
+        MicroModal.close('modal-1');
+    }
+    timezoneSelector.value = ""
+});
+
+
 
 MicroModal.init({
     onShow: modal => console.info(`${modal.id} is shown`), // [1]
-    onClose: modal => console.info(`${modal.id} is hidden`), // [2]
+    onClose: modal => { 
+        console.info(`${modal.id} is hidden`)
+        timezoneSelector.value = ""
+    }, // [2]
     openTrigger: 'data-micromodal-trigger', // [3]
     closeTrigger: 'data-micromodal-close', // [4]
     openClass: 'is-open', // [5]
