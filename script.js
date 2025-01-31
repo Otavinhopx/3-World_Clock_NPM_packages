76172d95278dfe1e9a2285c28a0c27cedea8eaac
Miguel0528 <miguel.araujo0528@gmail.com>
2025-01-30 22:06:54 -0300

    Utilização do Micromodal para mostrar ou esconder o modal

--- script.js
+++ script.js
@@ -14,3 +14,15 @@ setInterval(updateClock, 1000);
 updateClock();
 dateDay.innerHTML += date
 
+MicroModal.init({
+    onShow: modal => console.info(`${modal.id} is shown`), // [1]
+    onClose: modal => console.info(`${modal.id} is hidden`), // [2]
+    openTrigger: 'data-micromodal-trigger', // [3]
+    closeTrigger: 'data-micromodal-close', // [4]
+    openClass: 'is-open', // [5]
+    disableScroll: true, // [6]
+    disableFocus: false, // [7]
+    awaitOpenAnimation: false, // [8]
+    awaitCloseAnimation: false, // [9]
+    debugMode: true // [10]
+  });
